db91cc8764e6ba345286a875042c6054abf7e45b
Towerrrr <1611993104@qq.com>
2025-05-18 23:12:05 +0800

    通用布局开发

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader></GlobalHeader>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://yupi.icu.com" target="_blank"
+          >编程导航知识星球</a
+        ></a-layout-footer
+      >
+    </a-layout>
+  </div>
+</template>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 14px;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  text-align: center;
+}
+</style>
