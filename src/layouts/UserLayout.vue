1135c0c7e3434429d167557c7323e6b597afa032
Towerrrr <1611993104@qq.com>
2025-05-26 22:48:39 +0800

    登录页开发

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="userLayout">
     <a-layout style="min-height: 100vh">
-      <a-layout-header class="header"> 用户布局</a-layout-header>
+      <a-layout-header class="header">
+        <a-space>
+          <img src="../assets/logo.png" class="logo" />
+          <div>Kestrel OJ</div>
+        </a-space>
+      </a-layout-header>
       <a-layout-content class="content">
         <router-view />
       </a-layout-content>
@@ -16,21 +21,26 @@
 <script setup lang="ts"></script>
 <style scoped>
 #userLayout {
+  text-align: center;
+  background: url(https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png)
+    0% 0% / 100% 100%;
+}
+
+#userLayout .logo {
+  width: 64px;
+  height: 64px;
 }
 
 #userLayout .header {
-  margin-bottom: 16px;
-  box-shadow: 1px 1px 5px #eee;
+  margin-top: 16px;
 }
 
 #userLayout .content {
-  background: linear-gradient(to right, #bbb, #fff);
   margin-bottom: 16px;
   padding: 20px;
 }
 
 #userLayout .footer {
-  background: #efefef;
   padding: 14px;
   position: sticky;
   bottom: 0;
