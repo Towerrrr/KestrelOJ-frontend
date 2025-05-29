b5fe3d34d0ea5d9f01a0c4c52663dabec6fa6cff
Towerrrr <1611993104@qq.com>
2025-05-29 16:36:08 +0800

    解决Babel插件配置错误

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [
+    // 强制 babel 转译 monaco-editor
+    "monaco-editor",
+  ],
   chainWebpack(config) {
     config.plugin("monaco").use(new MonacoWebpackPlugin());
   },
