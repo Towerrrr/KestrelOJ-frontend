4e1462c52916ac8a1596bf929591d6d65d75b82a
Towerrrr <1611993104@qq.com>
2025-09-08 23:55:04 +0800

    refactor: 引用警告处理

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import ExampleView from "../views/ExampleView.vue";
+import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "@/router/routes";
 
 const router = createRouter({
